de04e65ac49f369edc2e20b42f000aca817a91f0
mahmedh <m7md.7amed94@gmail.com>
2020-08-04 22:18:01 +0200

    yarab nengez

--- src/test/java/Login/LearnRebase.java
+++ src/test/java/Login/LearnRebase.java
@@ -1,4 +1,16 @@
 package Login;
+import org.testng.annotations.Test;
 
 public class LearnRebase {
+    @Test
+    public void ayHaga()
+    {
+
+    }
+    @Test
+    public void engez()
+    {
+
+    }
+
 }
